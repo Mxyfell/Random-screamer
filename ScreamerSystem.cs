81f2d90d6fcd2b1e69dd156b77e0236b67cd162e
Mxyfell <Mxyfell@DESKTOP-2J6KJJA>
2025-09-28 23:05:27 +0300

    Добавьте файлы проекта.

---
+++ ScreamerSystem.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Screamer
+{
+    public class ScreamerSystem : MonoBehaviour
+    {
+        private GameObject canvasScreamer;
+        private AudioSource audioScreamer;
+        private bool Generate = true;
+
+        void Start()
+        {
+            SceneManager.sceneLoaded += onSceneLoaded;
+            LoadScreamerResources();
+        }
+
+        void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= onSceneLoaded;
+        }
+
+        void onSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            LoadScreamerResources();
+        }
+
+        void LoadScreamerResources()
+        {
+            var prefab = Resources.FindObjectsOfTypeAll<GameObject>()
+            .FirstOrDefault(go => go.name == "GloboAsesino");
+            if (prefab != null)
+            {
+                GameObject balloon = Instantiate(prefab);
+                balloon.name = "";
+                Destroy(balloon.GetComponent<GloboAsesino>());
+                canvasScreamer = balloon.transform.Find("CanvasScreamer").gameObject;
+                audioScreamer = canvasScreamer.GetComponent<AudioSource>();
+
+                if (canvasScreamer != null)
+                    canvasScreamer.SetActive(false);
+            }
+        }
+
+        void Update()
+        {
+            if (Generate)
+            {
+                int chance = Random.Range(0, 20001);
+                if (chance == 5)
+                {
+                    StartCoroutine(Screamer());
+                }
+            }
+        }
+
+        IEnumerator Screamer()
+        {
+            if (canvasScreamer != null && audioScreamer != null)
+            {
+                Generate = false;
+                canvasScreamer.SetActive(value: true);
+                audioScreamer.Play();
+                yield return new WaitForSeconds(2f);
+                canvasScreamer.SetActive(value: false);
+                Generate = true;
+            }
+        }
+    }
+}
---
+++ Setup.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Screamer
+{
+    [BepInPlugin("ru.MxyfellKek.Screamer", "Screamer", "1.0.0")]
+    public class Setup : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == "menus") return;
+
+            GameObject screamer = new GameObject("");
+            screamer.AddComponent<ScreamerSystem>();
+        }
+    }
+}
